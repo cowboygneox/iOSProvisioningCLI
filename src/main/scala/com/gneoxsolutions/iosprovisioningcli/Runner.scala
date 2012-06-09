be4dec986454c6f547665af20d1bcd5f7bf749d6
Sean Freitag <freitag.sean@gmail.com>
2012-06-08 21:13:05 -0500

    Adding a test for 'list development profiles'

--- src/main/scala/com/gneoxsolutions/iosprovisioningcli/Runner.scala
+++ src/main/scala/com/gneoxsolutions/iosprovisioningcli/Runner.scala
@@ -3,3 +3,15 @@ package com.gneoxsolutions.iosprovisioningcli
 object Runner extends App {
 
 }
+
+trait SiteDriver {
+  def listDevelopmentProfiles()
+}
+
+class Runner {
+  self: SiteDriver =>
+
+  def << (commandString: String) {
+    listDevelopmentProfiles()
+  }
+}
--- src/test/scala/com/gneoxsolutions/iosprovisioningcli/RunnerTest.scala
+++ src/test/scala/com/gneoxsolutions/iosprovisioningcli/RunnerTest.scala
@@ -1,5 +1,18 @@
 package com.gneoxsolutions.iosprovisioningcli
 
 class RunnerTest extends org.specs2.mutable.Specification {
+  "A Runner" should {
+    var listDevelopmentProfilesHappend = false
 
+    val runner = new Runner() with SiteDriver {
+      def listDevelopmentProfiles() {
+        listDevelopmentProfilesHappend = true
+      }
+    }
+    
+    "List development provisioning profiles" in {
+      runner << "list development profiles"
+      listDevelopmentProfilesHappend must beEqualTo(true)
+    }
+  }
 }
