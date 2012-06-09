81482936c69efb86228c264bcb9037dae2d9338d
Sean Freitag <freitag.sean@gmail.com>
2012-06-08 21:43:12 -0500

    Adding test for adding a device

--- src/main/scala/com/gneoxsolutions/iosprovisioningcli/Runner.scala
+++ src/main/scala/com/gneoxsolutions/iosprovisioningcli/Runner.scala
@@ -7,18 +7,32 @@ object Runner extends App {
 trait SiteDriver {
   def listDevelopmentProfiles()
   def listDistributionProfiles()
+  def addNewDevice(deviceName: String, deviceUuid: String)
 }
 
 class Runner {
   self: SiteDriver =>
 
   def << (commandString: String) {
-    commandString match {
-      case "list development profiles" => {
+    self << commandString.split(" ")
+  }
+
+  def << (args: Array[String]) {
+    args match {
+      case Array("list", "development", "profiles") => {
         listDevelopmentProfiles()
       }
-      case "list distribution profiles" => {
+      case Array("list", "distribution", "profiles") => {
         listDistributionProfiles()
+      }
+      case Array("add", "device", _*) => {
+        val remainingArgs = args.slice(2, args.length)
+        val deviceName = remainingArgs.slice(0, remainingArgs.length - 1).mkString(" ")
+        val deviceUuid = remainingArgs(remainingArgs.length - 1)
+        addNewDevice(deviceName, deviceUuid)
+      }
+      case _ => {
+
       }
     }
   }
--- src/test/scala/com/gneoxsolutions/iosprovisioningcli/RunnerTest.scala
+++ src/test/scala/com/gneoxsolutions/iosprovisioningcli/RunnerTest.scala
@@ -4,10 +4,13 @@ class RunnerTest extends org.specs2.mutable.Specification {
   "A Runner" should {
     var listDevelopmentProfilesHappened = false
     var listDistributionProfilesHappened = false
+    var caughtDeviceName = ""
+    var caughtDeviceUuid = ""
 
     val runner = new Runner() with SiteDriver {
       def listDevelopmentProfiles() { listDevelopmentProfilesHappened = true }
       def listDistributionProfiles() { listDistributionProfilesHappened = true }
+      def addNewDevice(deviceName: String, deviceUuid: String) { caughtDeviceName = deviceName; caughtDeviceUuid = deviceUuid }
     }
 
     "List development provisioning profiles" in {
@@ -18,6 +21,12 @@ class RunnerTest extends org.specs2.mutable.Specification {
     "List distribution provisioning profiles" in {
       runner << "list distribution profiles"
       listDistributionProfilesHappened must beEqualTo(true)
+    }
+
+    "Add new device" in {
+      runner << "add device a new phone abcdef1234567890"
+      caughtDeviceName must beEqualTo("a new phone")
+      caughtDeviceUuid must beEqualTo("abcdef1234567890")
     }
   }
 }
